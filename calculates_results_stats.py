09232508195be7bbd881d8da3df711fbd4d8379c
Pradeep <ppujari@github.com>
2019-10-11 23:11:32 -0700

    small calculation fix

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -100,7 +100,7 @@ def calculates_results_stats(results_dic):
         #           
         # Pet Image Label is a Dog AND Labels match- counts Correct Breed
         if results_dic[key][3] == 1:
-            if results_dic[key][4] == 1:
+            if results_dic[key][2] == 1:
                 results_stats_dic['n_correct_breed'] += 1
  
         
